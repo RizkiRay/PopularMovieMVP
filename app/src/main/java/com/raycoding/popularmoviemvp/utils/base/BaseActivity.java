736f3f13d303ec52bceac45d493c6b0be3fa2258
Rizki Raynaldy <rizkirayraynaldy@gmail.com>
2018-05-04 15:38:43 +0700

    Create base package and Base Classes

---
+++ app/src/main/java/com/raycoding/popularmoviemvp/utils/base/BaseActivity.java
@@ -0,0 +1,8 @@
+package com.raycoding.popularmoviemvp.utils.base;
+
+/**
+ * Created by ray <rizkirayraynaldy@gmail.com> on 04/05/18.
+ */
+
+public abstract class BaseActivity {
+}
---
+++ app/src/main/java/com/raycoding/popularmoviemvp/utils/base/MvpView.java
@@ -0,0 +1,24 @@
+package com.raycoding.popularmoviemvp.utils.base;
+
+import android.support.annotation.StringRes;
+
+/**
+ * Created by ray <rizkirayraynaldy@gmail.com> on 04/05/18.
+ */
+
+public interface MvpView {void showLoading();
+
+    void hideLoading();
+    
+    void onError(@StringRes int resId);
+
+    void onError(String message);
+
+    void showMessage(String message);
+
+    void showMessage(@StringRes int resId);
+
+    boolean isNetworkConnected();
+
+    void hideKeyboard();
+}
